12b95ea5c190f8b0cd76e3de2a0ddfe3e1132447
Sukhorukov Dmitry <DimaJTX@mail.ru>
2021-10-14 21:37:47 +0700

    DS:init repo

---
+++ Aoutontification/Aoutontification/Data/Model/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aoutontification.Data.Model
+{
+    public class Account
+    {      
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        [EmailAddress(ErrorMessage = "Некорректный адрес")] 
+        public string EmailAdress { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumb { get; set; }
+
+        
+    }
+}
---
+++ Aoutontification/Aoutontification/Pages/CounterViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Aoutontification.Data.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Aoutontification.Pages
+{
+
+    public class CounterViewMode: ComponentBase
+    {
+        public Account Data = new Account();
+        public List<Account> Spisok = new List<Account>();
+        public string Login(Account param1)
+        {
+            string json = File.ReadAllText(@"C:\Users\stellpro\Desktop\database.json");
+            Spisok = JsonSerializer.Deserialize<List <Account>>(json);
+            for (int i = 0; i < Spisok.Count; i++)
+            {
+                if ((param1.EmailAdress == Spisok[i].EmailAdress) && (param1.Password == Spisok[i].Password))
+                {
+                    return "Access";
+                }
+                else
+                {
+                    return "Incorrect Data";
+                }
+
+            }
+
+            return "Incorrect Data";
+        }
+        public void Reg(Account param1)
+        {
+            for (int i = 0; i < Spisok.Count; i++)
+            {
+                if (param1 == Spisok[i])
+                {
+                    break;
+                }
+                else
+                {
+                    Spisok.Add(param1);
+                }
+             }
+            string json = JsonSerializer.Serialize(Spisok);
+            File.WriteAllText(@"C:\Users\stellpro\Desktop\database.json", json);
+            StateHasChanged();
+        }
+        public bool Flag = false;
+        public void ChangeFlag()
+        {
+            if (Flag == false)
+            {
+                Flag = true;
+            }
+            else
+            {
+                Flag = false;
+            }
+        }
+    }
+}
